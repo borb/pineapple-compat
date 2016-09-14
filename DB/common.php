3187416cd52c459dda8d7a2f30dda2dd076d5632
Rob Andrews <rob.andrews@mayden.co.uk>
2016-09-14 12:57:37 +0100

    alias function toSleep(), which was removed

--- DB/common.php
+++ DB/common.php
@@ -1,4 +1,9 @@
 <?php
 class DB_common extends \Mayden\Pineapple\DB\Driver\Common
 {
+    // removed in pineapple, deprecated in DB before that
+    public function toSleep()
+    {
+        return $this->__toSleep();
+    }
 }
