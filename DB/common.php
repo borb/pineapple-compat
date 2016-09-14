ca20e60eb80b44c4f219ee3cfec9d2840ffeb582
Rob Andrews <rob.andrews@mayden.co.uk>
2016-09-14 14:06:55 +0100

    copy description docblock from DB\Driver\Common

--- DB/common.php
+++ DB/common.php
@@ -1,7 +1,13 @@
 <?php
 class DB_common extends \Mayden\Pineapple\DB\Driver\Common
 {
-    // removed in pineapple, deprecated in DB before that
+    /**
+     * DEPRECATED:  String conversion method
+     *
+     * @return string  a string describing the current PEAR DB object
+     *
+     * @deprecated Method deprecated in Release 1.7.0
+     */
     public function toSleep()
     {
         return $this->__toSleep();
